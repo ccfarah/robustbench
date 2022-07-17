b3cd63915008984660e55a41dbd186991b4af942
cfarah <christopher.farah@anno.ai>
2022-07-16 23:59:06 -0400

    test_model

--- test_model.py
+++ test_model.py
@@ -1,11 +1,19 @@
-from cgi import test
-from dataclasses import dataclass
-from pyexpat import model
+from robustbench.data import load_cifar10
+from robustbench.utils import load_model
+import foolbox as fb
 
+# class ModelTester:
+#     def __init__(self, model, data, test):
+#         self.model = model
+#         self.data = data
+#         self.test = tests
 
-class ModelTester:
-    def __init__(self, model, data, test):
-        self.model = model
-        self.data = data
-        self.test = test
+def test_model():
+    x_test, y_test = load_cifar10(n_examples=50)
+    model = load_model(model_name='Carmon2019Unlabeled', dataset='cifar10', threat_model='Linf')
+    fmodel = fb.PyTorchModel(model, bounds=(0, 1))
+    _, advs, success = fb.attacks.LinfPGD()(fmodel, x_test.to('cuda:0'), y_test.to('cuda:0'), epsilons=[8/255])
+    print('Robust accuracy: {:.1%}'.format(1 - success.float().mean()))
 
+if __name__ == '__main__':
+    test_model()
