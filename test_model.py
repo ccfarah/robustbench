d6e0e74abedb92e6e1e97ea8a6765847316ccec0
cfarah <christopher.farah@anno.ai>
2022-07-17 22:16:24 -0400

    corruption added.

--- test_model.py
+++ test_model.py
@@ -1,6 +1,9 @@
 from robustbench.data import load_cifar10
+from robustbench.data import load_cifar10c
+from robustbench.utils import clean_accuracy
 from robustbench.utils import load_model
 import foolbox as fb
+from autoattack import AutoAttack
 
 # class ModelTester:
 #     def __init__(self, model, data, test):
@@ -14,6 +17,19 @@ def test_model():
     fmodel = fb.PyTorchModel(model, bounds=(0, 1))
     _, advs, success = fb.attacks.LinfPGD()(fmodel, x_test.to('cuda:0'), y_test.to('cuda:0'), epsilons=[8/255])
     print('Robust accuracy: {:.1%}'.format(1 - success.float().mean()))
+    adversary = AutoAttack(model, norm='Linf', eps=8/255, version='custom', attacks_to_run=['apgd-ce', 'apgd-dlr'])
+    adversary.apgd.n_restarts = 1
+    x_adv = adversary.run_standard_evaluation(x_test, y_test)
+
+def corruption():
+    corruptions = ['fog']
+    x_test, y_test = load_cifar10c(n_examples=1000, corruptions=corruptions, severity=5)
+    for model_name in ['Standard', 'Engstrom2019Robustness', 'Rice2020Overfitting',
+                    'Carmon2019Unlabeled']:
+        model = load_model(model_name, dataset='cifar10', threat_model='Linf')
+        acc = clean_accuracy(model, x_test, y_test)
+        print(f'Model: {model_name}, CIFAR-10-C accuracy: {acc:.1%}')
 
 if __name__ == '__main__':
     test_model()
+    corruption()
